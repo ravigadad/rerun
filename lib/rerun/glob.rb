869a24ee73c28a3a232aeeab568aedda4188d051
Ravi Gadad <ravi@renewfund.com>
2016-06-30 16:29:10 -0700

    Refactor Glob.nested? method
    This might be too "clever" of a refactor, but the logic seems pretty
    straightforward to me.  I'd defer to a code reviewer on the tradeoff
    of line count vs. readability here.

--- lib/rerun/glob.rb
+++ lib/rerun/glob.rb
@@ -23,12 +23,7 @@ module Rerun
 
     def self.nested?(arr)
       unclosed_braces = arr.inject(0) { |count, char|
-        if char == "{"
-          count += 1
-        elsif char == "}"
-          count -= 1
-        end
-        count
+        count + { "{" => 1, "}" => -1 }.fetch(char, 0)
       }
       unclosed_braces > 0
     end
